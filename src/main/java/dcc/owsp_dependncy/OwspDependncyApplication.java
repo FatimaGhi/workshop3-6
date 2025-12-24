945a98c7c3fbcc0c1199e8ef402d1caa83aed37d
FatimaGhi <fatimazahraeelghiouan69@gmail.com>
2025-12-24 21:14:28 +0100

    first commit

---
+++ src/main/java/dcc/owsp_dependncy/OwspDependncyApplication.java
@@ -0,0 +1,13 @@
+package dcc.owsp_dependncy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwspDependncyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwspDependncyApplication.class, args);
+    }
+
+}
